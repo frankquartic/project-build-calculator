a023ece92df44c2e1a83f5bf2204118e02d4c01f
frankdede <guanqi@ualberta.ca>
2019-09-04 00:35:47 -0400

    Updates

---
+++ precache-manifest.2f0aa27591921fe3e5d602820c4ce287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e178131b446bef5c3cb208ab19fbdbba",
+    "url": "/project-build-calculator/index.html"
+  },
+  {
+    "revision": "6343ad7d0de8611613a3",
+    "url": "/project-build-calculator/static/css/main.4221c82b.chunk.css"
+  },
+  {
+    "revision": "8586b5cdf4d8f074d2c4",
+    "url": "/project-build-calculator/static/js/2.35eb68fc.chunk.js"
+  },
+  {
+    "revision": "6343ad7d0de8611613a3",
+    "url": "/project-build-calculator/static/js/main.dc0f5687.chunk.js"
+  },
+  {
+    "revision": "d4785208704866dbd2a0",
+    "url": "/project-build-calculator/static/js/runtime~main.4ce14645.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-build-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
